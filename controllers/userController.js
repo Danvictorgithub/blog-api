713232998b467631415ab6d1ecce44124a20e34b
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-02-07 15:13:39 +0800

    Added bcryptjs:updated controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-
+const {body, validationResult} = require('express-validator');
 exports.login_GET = (req,res) => {
 	res.json({message:"Not Yet Implemented"});
 };
@@ -9,6 +9,16 @@ exports.login_POST = (req,res) => {
 exports.signup_GET = (req,res) => {
 	res.json({message:"Not Yet Implemented"});
 };
-exports.signup_POST = (req,res) => {
-	res.json({message:"Not Yet Implemented"});
-};
+exports.signup_POST = [
+	body('username').trim().isLength({min:8}).withMessage("Username must be more than or equal 8 characters").isLength({max:16}).withMessage("Username must be less than 16 characters").escape(),
+	body('password').trim().isLength({min:8}).withMessage("Password must be atleast 8 characters").isLength({max:128}).withMessage("Password exceeds 128 character limit").escape(),
+	(req,res,next) => {
+		console.log(req.body);
+		const errors = validationResult(req);
+		if(!errors.isEmpty()) {
+			res.json({message:errors.array()});
+			return;
+		}
+		res.json({message:"Success!"});
+	}	
+];
