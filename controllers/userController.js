1e1c028f7f868f944e10314e14d7be3d6c2e6e9c
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-02-08 19:06:53 +0800

    Added more comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,9 @@ exports.login_POST = (req,res,next) => {
 	//					 - success then login user with JWT token used for user authentication to protected routes
 	
 	passport.authenticate('local',{session:false},(err,user,info) => {
+
 		// session is set to false since API is used for authentication
+		
 		if (err || !user) {
 			return res.status(400).json({
 				message: "Something is not right",
@@ -42,7 +44,9 @@ exports.signup_GET = (req,res) => {
 	res.json({message:"Not Yet Implemented"});
 };
 exports.signup_POST = [
+
 	// check('params').sanitationRules({params:paramsValue}) Sanitizes req.body.<formData>
+	
 	check('username').trim().isLength({min:8}).withMessage("Username must be more than or equal 8 characters").isLength({max:16}).withMessage("Username must be less than 16 characters").escape(),
 	check('password').isAlphanumeric().withMessage("Password must be Alphanumeric").isLength({min:8}).withMessage("Password must be atleast 8 characters").isLength({max:128}).withMessage("Password exceeds 128 character limit").escape(),
 	(req,res,next) => {
@@ -56,6 +60,9 @@ exports.signup_POST = [
 			if (err) {
 				return next(err);
 			}
+
+			// Checks if user already exist in the database, otherwise create new User with hashed password
+			
 			if (result == null) {
 				bcrypt.hash(req.body.password,12,(err,hashedPassword) => {
 					if (err) {
