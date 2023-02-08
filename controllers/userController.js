749bde34348d4418327a5273b3d344fdcb04a9a1
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-02-08 19:04:08 +0800

    Added Comment for future reference

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,9 +10,11 @@ exports.login_GET = (req,res,next) => {
 };
 
 exports.login_POST = (req,res,next) => {
+
 	// passport.authenticate('local',{session:false},(err,user,info) =>{authentication})(req,res) //It is an object constructor with double return values
 	// authentication -> - error or user doesn't exist returns statusCode 400 (Bad Request)
 	//					 - success then login user with JWT token used for user authentication to protected routes
+	
 	passport.authenticate('local',{session:false},(err,user,info) => {
 		// session is set to false since API is used for authentication
 		if (err || !user) {
@@ -40,9 +42,11 @@ exports.signup_GET = (req,res) => {
 	res.json({message:"Not Yet Implemented"});
 };
 exports.signup_POST = [
+	// check('params').sanitationRules({params:paramsValue}) Sanitizes req.body.<formData>
 	check('username').trim().isLength({min:8}).withMessage("Username must be more than or equal 8 characters").isLength({max:16}).withMessage("Username must be less than 16 characters").escape(),
 	check('password').isAlphanumeric().withMessage("Password must be Alphanumeric").isLength({min:8}).withMessage("Password must be atleast 8 characters").isLength({max:128}).withMessage("Password exceeds 128 character limit").escape(),
 	(req,res,next) => {
+		// Returns Error if request.body failed Sanitation process
 		const errors = validationResult(req);
 		if(!errors.isEmpty()) {
 			res.status(400).json({errors:errors.array()});
@@ -55,7 +59,7 @@ exports.signup_POST = [
 			if (result == null) {
 				bcrypt.hash(req.body.password,12,(err,hashedPassword) => {
 					if (err) {
-						return next(err);
+						return res.status(400).json({message:"Something is not right"});
 					}
 					const newUser = new User({
 						username:req.body.username,
@@ -65,14 +69,14 @@ exports.signup_POST = [
 						if (err) {
 							return next(err);
 						}
-						res.status(201).json({
+						return res.status(201).json({
 							statusCode:201,
 							message:"User successfully created"
 						});
 					});
 				});
 			} else {
-				res.status(400).json({
+				return res.status(400).json({
 					statusCode:400,
 					message:"Username Already Exist",
 				});
